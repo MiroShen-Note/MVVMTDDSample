a17b532036b3916f4b22ae3d179bc70ca3dc3bdc
LAPTOP-URKIRU4J\chtw1 <miro.job.research@gmail.com>
2023-11-07 01:05:31 +0800

    init mvvmtdd sample

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "miro.shen.research.mvvmtddsample"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "miro.shen.research.mvvmtddsample"
         minSdk = 29
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
