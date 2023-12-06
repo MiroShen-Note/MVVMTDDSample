45a99dce3384483b3b1b04c402d0f1640447dd53
Miro <miro.fii.foxconn@gmail.com>
2023-12-06 12:03:22 +0800

    init mvvmtdd sample

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,27 +33,41 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
 
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "11"
     }
 }
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("androidx.core:core-ktx:1.2.0")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.test.espresso:espresso-idling-resource:3.5.1")
+
+    val lifecycleVersion = "2.6.2"
+    val archVersion = "2.2.0"
+
+    implementation("androidx.lifecycle:lifecycle-view-model-ktx:$lifecycleVersion")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycleVersion")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycleVersion")
+    implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
 
     testImplementation("junit:junit:4.13.2")
-
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
+
+    implementation("androidx.test.espresso:espresso-idling-resource:3.5.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
+    testImplementation("io.mockk:mockk:1.13.8")
+
+    testImplementation("androidx.arch.core:core-testing:$archVersion")
+
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:okhttp:4.7.2")
 }
---
+++ app/src/main/java/miro/shen/research/mvvmtddsample/ResultActivity.kt
@@ -0,0 +1,14 @@
+package miro.shen.research.mvvmtddsample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ResultActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+
+        val id = intent.getStringExtra("ID")
+    }
+}
